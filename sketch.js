5b2da795d9b5b4800b5d8f0016d189bc4ac480c2
PranavSomayajiSastry <kittupranav27@outlook.com>
2019-12-27 17:43:48 +0530

    learnt about '.set''.database''.ref'

--- sketch.js
+++ sketch.js
@@ -1,29 +1,55 @@
-var ball;
+var hypnoticBall;
+var database,position;
+function setup()
+{
+    database = firebase.database();
 
-function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+   hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red";
+
+    var hypnoticBallPosition = database.ref('ball/position');
+    hypnoticBallPosition.on("value",readPosition,showError);
 }
 
-function draw(){
+function draw()
+{
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+    if(keyDown(LEFT_ARROW))
+    {
+       writePosition(-1,0);
     }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+    else if(keyDown(RIGHT_ARROW))
+    {
+       writePosition(1,0);
     }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+    else if(keyDown(UP_ARROW))
+    {
+       writePosition(0,-1);
     }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+    else if(keyDown(DOWN_ARROW))
+    {
+       writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y)
+{
+    database.ref('ball/position').set({
+        'x' : position.x + x,
+        'y' : position.y + y,
+    })
+    
+}
+function readPosition(data)
+{
+    position = data.val();
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+}
+
+function showError()
+{
+    console.log("error in writing into the database"); 
 }
